0418e887bb8f78373ecdb187f9dcb7129b48e0c4
Liv Rickey <liv@snekbian.home.local>
2025-06-22 20:38:33 -0500

    collars

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Cascadia Code NF:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /* How to align the content in the window when the size of the terminal
@@ -107,32 +107,33 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+    "#191724", 
+    "#eb6f92", 
+    "#9ccfd8", 
+    "#f6c177", 
+    "#31748f", 
+    "#c4a7e7", 
+    "#ebbcba", 
+    "#e0def4", 
+
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
+    "#6e6a86", 
+    "#eb6f92", 
+    "#9ccfd8", 
+    "#f6c177", 
+    "#31748f", 
+    "#c4a7e7", 
+    "#ebbcba", 
+    "#e0def4", 
+	
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#e0def4",
+	"#e0def4",
+	"#e0def4", /* default foreground colour */
+	"#26233a", /* default background colour */
 };
 
 
